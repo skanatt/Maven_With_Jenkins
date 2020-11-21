f20abcda725f16bba9692f16a8f70a22cff33a93
shruti <shruti.kanatt@gmail.com>
2020-11-21 16:48:58 +0530

    initial commit

---
+++ src/main/java/com/lti/App.java
@@ -0,0 +1,17 @@
+package com.lti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void m1(){
+    	System.out.println("Calling obj of App class!");
+    }
+}
